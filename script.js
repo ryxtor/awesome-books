c2ee4134d778d8858732750e4ee36806f13409f6
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-02-07 17:17:39 -0300

    Add contructor and variables

---
+++ script.js
@@ -0,0 +1,43 @@
+const books = {};
+
+const inputTitle = document.querySelector('title');
+const inputAuthor = document.querySelector('author');
+const btnAdd = document.querySelector('btn-add');
+const bookList = document.querySelector('list');
+
+let bookCollection = JSON.parse(localStorage.getItem(bookAuthor)) || [];
+
+function awesomeBooks(title, author, id) {
+this.title = title;
+this.author = author;
+this.id = id;
+}
+
+function displayBooks() {
+  if (ul.querySelectorAll('li')) {
+    Array.from(ul.querySelectorAll('li')).forEach((bookContainer) => {
+      ul.removeChild(bookContainer);
+    });
+  }
+}
+
+bookCollection.forEach((book) => {
+  const li = document.createElement('li');
+  li.className = 'book';
+  const bookAuthor = document.createElement('p');
+  bookAuthor.className = 'book-author';
+  const bookTitle = document.createElement('p');
+  bookTitle.className = 'book-title';
+  const deletebtn = document.createElement('button');
+  deletebtn.id = book.id;
+  deletebtn.className = 'remove-btn';
+  deletebtn.textContent = 'Remove';
+  bookAuthor.textContent = book.author;
+  bookTitle.textContent = book.title;
+  li.appendChild(bookAuthor);
+  li.appendChild(bookTitle);
+  li.appendChild(deletebtn);
+  ul.appendChild(li);
+});
+
+displayBooks();
